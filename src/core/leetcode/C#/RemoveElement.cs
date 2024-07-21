489665fc1245a3b5abb70468825d205db0f75bff
Relsola <sola001018@gmail.com>
2024-07-21 20:56:26 +0800

    leetcode 移除元素

---
+++ src/core/leetcode/C#/RemoveElement.cs
@@ -0,0 +1,18 @@
+﻿// https://leetcode.cn/problems/remove-element/ 移除元素
+public class RemoveElement
+{
+    public int Main(int[] nums, int val)
+    {
+        int slow = 0;
+        for (int fast = 0; fast < nums.Length; fast++)
+        {
+            if (val != nums[fast])
+            {
+                nums[slow++] = nums[fast];
+            }
+        }
+
+        return slow;
+    }
+}
+
---
+++ src/core/leetcode/C/removeElement.c
@@ -0,0 +1,12 @@
+// https://leetcode.cn/problems/remove-element/ ????
+int removeElement(int* nums, int numsSize, int val) {
+	int slow = 0;
+	for (int fast = 0; fast < numsSize; fast++)
+	{
+		if (nums[fast] != val) {
+			nums[slow++] = nums[fast];
+		}
+	}
+
+	return slow;
+}
