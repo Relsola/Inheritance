f6d14a2ec782cef6548574de127b23d66a995b3b
Relsola <sola001018@gmail.com>
2024-07-21 23:48:34 +0800

    拼图游戏路由入口

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 function Games() {
-  return <h1>Games</h1>;
+  const base = import.meta.env.VITE_BASE_URL;
+
+  return (
+    <>
+      <h1>Games</h1>
+      <Link
+        to={base + 'games/puzzle'}
+        className="flex-center w30 h11 rounded mx2 color-[#333333] color-active hover:bg-[#f5f5f5]"
+      >
+        <h3>Puzzle</h3>
+      </Link>
+    </>
+  );
 }
 
 export default Games;
