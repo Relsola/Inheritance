ae24ead6b55bf078481849fe0e769efd76e51d28
Relsola <sola001018@gmail.com>
2024-07-21 20:43:44 +0800

    leetcode 二分查找

---
+++ src/core/leetcode/C#/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.cn/problems/binary-search/ 二分查找
+public class TwoSum
+{
+  public int Main(int[] nums, int target)
+  {
+    int left = 0, right = nums.Length - 1;
+
+    while (left <= right)
+    {
+      int mid = (left + right) >> 1;
+      int num = nums[mid];
+      if (num == target)
+      {
+        return mid;
+      }
+      else if (num < target)
+      {
+        left = mid + 1;
+      }
+      else
+      {
+        right = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+}
