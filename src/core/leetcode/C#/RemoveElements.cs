5cc9a026f6bd898eaa93bb76c8229e9d30144bff
Relsola <sola001018@gmail.com>
2024-07-23 21:36:00 +0800

    leetcode 203移除链表元素

---
+++ src/core/leetcode/C#/RemoveElements.cs
@@ -0,0 +1,25 @@
+﻿namespace NEXT.LeetCode;
+
+internal class RemoveElements
+{
+    public ListNode? Main(ListNode head, int val)
+    {
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+
+        while (cur.next != null)
+        {
+            if (cur.next.val == val)
+            {
+                cur.next = cur.next.next;
+            }
+            else
+            {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
+
