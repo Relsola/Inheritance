640a538b50dc3309bea4f0f604f09291ad803190
Relsola <sola001018@gmail.com>
2024-08-05 00:44:39 +0800

    优化拼图游戏，增加动画

--- src/pages/games/puzzle/index.tsx
+++ src/pages/games/puzzle/index.tsx
@@ -1,79 +1,119 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { shuffle } from '@/utils';
 import { Button } from '@ui';
 
 function Puzzle() {
-  const baseArray = Array.from({ length: 9 }, (_, i) => i + 1);
+  const contentBox = useRef<HTMLDivElement>(null);
 
-  const [divS, setDivS] = useState<number[]>(baseArray);
+  /** 定位坐标 控制定位表现出动画 */
+  const content = [
+    [0, 0],
+    [0, 100],
+    [0, 200],
+    [100, 0],
+    [100, 100],
+    [100, 200],
+    [200, 0],
+    [200, 100],
+    [200, 200]
+  ];
 
-  const shuffle = (array: any[]) => {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  /** 移动拼图 */
+  function move(num: number) {
+    if (num === 8 || !contentBox.current || !contentBox.current.childNodes) {
+      return;
     }
-    return shuffled;
-  };
 
-  const isSolvable = (array: any[]) => {
+    const blankDiv = contentBox.current.childNodes[8] as HTMLDivElement;
+    const activeDiv = contentBox.current.childNodes[num] as HTMLDivElement;
+
+    const { top: blankTop, left: blankLeft } = blankDiv.style;
+    const { top: activeTop, left: activeLeft } = activeDiv.style;
+
+    const topAbs = Math.abs(parseInt(blankTop) - parseInt(activeTop));
+    const leftAbs = Math.abs(parseInt(blankLeft) - parseInt(activeLeft));
+
+    if (topAbs === 100 && leftAbs === 0) {
+      blankDiv.style.top = activeTop;
+      activeDiv.style.top = blankTop;
+    } else if (topAbs === 0 && leftAbs === 100) {
+      blankDiv.style.left = activeLeft;
+      activeDiv.style.left = blankLeft;
+    }
+  }
+
+  /** 逆序数为偶数拼图才有解  */
+  const isSolvable = (arr: number[]) => {
+    const length = arr.length;
     let count = 0;
-    for (let i = 0; i < array.length; i++) {
-      for (let j = i + 1; j < array.length; j++) {
-        if (array[i] > array[j]) {
-          count++;
-        }
-      }
-    }
+
+    for (let i = 0; i < length; i++)
+      for (let k = i + 1; k < length; k++) if (arr[i] > arr[k]) count++;
+
     return count % 2 === 0;
   };
 
-  const moveDiv = (num: number) => {
-    const index = divS.indexOf(num);
-    const blankIndex = divS.indexOf(9);
-    const diff = Math.abs(index - blankIndex);
-    if (diff === 1 || diff === 3) {
-      const newDivS = [...divS];
-      newDivS[blankIndex] = num;
-      newDivS[index] = 9;
-      setDivS(newDivS);
+  /** 开始游戏 */
+  function start() {
+    if (!contentBox.current || !contentBox.current.childNodes) return;
+
+    const base = Array.from({ length: 9 }, (_, i) => i);
+
+    while (true) {
+      const shuffled = shuffle(base);
+      if (isSolvable(shuffled)) {
+        contentBox.current.childNodes.forEach((div, index) => {
+          const [top, left] = content[shuffled[index]];
+          (div as HTMLDivElement).style.top = `${top}px`;
+          (div as HTMLDivElement).style.left = `${left}px`;
+        });
+        break;
+      }
     }
-  };
+  }
+
+  /** 重置拼图 */
+  function reset() {
+    if (!contentBox.current || !contentBox.current.childNodes) return;
+
+    contentBox.current.childNodes.forEach((div, index) => {
+      const [top, left] = content[index];
+      (div as HTMLDivElement).style.top = `${top}px`;
+      (div as HTMLDivElement).style.left = `${left}px`;
+    });
+  }
+
+  /** 拼图求解 */
+  function solve() {}
 
   return (
     <>
       <h1>Puzzle</h1>
       <p>Under construction...</p>
 
-      <div className="w75 h75 grid grid-cols-3 grid-rows-3">
-        {divS.map(div => (
+      <div ref={contentBox} className="w75 h75 relative">
+        {content.map((pos, index) => (
           <div
-            key={div}
-            className={`${div === 9 ? '' : 'bg-sky'} text-center leading-25`}
-            onClick={() => moveDiv(div)}
+            key={index}
+            style={{ top: `${pos[0]}px`, left: `${pos[1]}px` }}
+            className={`${index === 8 ? '' : 'bg-sky cursor-pointer'}
+              w24 h24 text-center leading-25 absolute transition-all`}
+            onClick={() => move(index)}
           >
-            {div}
+            {index}
           </div>
         ))}
       </div>
 
-      <Button
-        classname="w20 m-2"
-        onClick={() => {
-          while (true) {
-            const shuffled = shuffle(divS);
-            if (isSolvable(shuffled)) {
-              setDivS(shuffled);
-              break;
-            }
-          }
-        }}
-      >
+      <Button classname="w20 m-2" onClick={start}>
         开始
       </Button>
-      <Button classname="w20 mx-4" onClick={() => setDivS(baseArray)}>
+      <Button classname="w20 mx-4" onClick={reset}>
         重置
       </Button>
-      <Button classname="w20">求解</Button>
+      <Button classname="w20" onClick={solve}>
+        求解
+      </Button>
     </>
   );
 }
