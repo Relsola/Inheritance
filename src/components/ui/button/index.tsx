ff506bda84d1e473284004d50ab2c407dbbd0098
Relsola <sola001018@gmail.com>
2024-08-05 00:32:20 +0800

    优化触摸样式

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -8,7 +8,8 @@ export default function Button(props: ButtonProps) {
   const { children, onClick, classname = '' } = props;
   return (
     <button
-      className={`${classname} h8 text-xs border-none rounded-md bg-[#0047f112] text-[#002BB7C5] hover:bg-[#D5EFFF] active:bg-[#ACD8FC] px3`}
+      className={`${classname} cursor-pointer h8 text-xs border-none rounded-md
+       bg-[#0047f112] text-[#002BB7C5] hover:bg-[#D5EFFF] active:bg-[#ACD8FC] px3`}
       onClick={onClick}
     >
       {children}
