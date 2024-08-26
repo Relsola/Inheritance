336cba5994e826c052c6c839c419d66ef87f4e70
Relsola <sola001018@gmail.com>
2024-08-26 20:47:47 +0800

    C++ 两个数组的交集

---
+++ src/core/leetcode/C++/intersection.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        unordered_set<int> result;
+
+        int hash[1001] = {0};
+
+        for (int num : nums1)
+            hash[num] = 1;
+
+        for (int num : nums2)
+            if (hash[num] == 1)
+                result.insert(num);
+
+        return vector<int>(result.begin(), result.end());
+    }
+};
